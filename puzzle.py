c633e2dcd369a41c88907e93e9cf31c73539ca55
iamthewalrus67 <bodya.ru02@gmail.com>
2021-02-03 22:06:36 +0200

    Initial commit

---
+++ puzzle.py
@@ -0,0 +1,69 @@
+def validate_board(board: list) -> bool:
+    '''
+    >>> validate_board(["**** ****", "***1 ****", "**  3****", "* 4 1****",\
+ "     9 5 ", " 6  83  *", "3   1  **", "  8  2***", "  2  ****"])
+    False
+    '''
+    return row_check(board) and column_check(board) and color_check(board)
+
+
+def row_check(board: list) -> bool:
+    '''
+    >>> row_check(["**** ****", "***1 ****", "**  3****", "* 4 1****",\
+ "     9 5 ", " 6  83  *", "3   1  **", "  8  2***", "  2  ****"])
+    True
+    '''
+    numbers = '123456789'
+    for row in board:
+        for number in numbers:
+            if row.count(number) > 1:
+                return False
+
+    return True
+
+
+def column_check(board: list) -> bool:
+    '''
+    >>> column_check(["**** ****", "***1 ****", "**  3****", "* 4 1****",\
+ "     9 5 ", " 6  83  *", "3   1  **", "  8  2***", "  2  ****"])
+    False
+    '''
+    numbers = '123456789'
+    for i in range(len(board[0])):
+        for _ in board:
+            column = ''
+            for row in board:
+                column += row[i]
+            for number in numbers:
+                if column.count(number) > 1:
+                    return False
+
+    return True
+
+
+def color_check(board: list) -> bool:
+    '''
+    >>> color_check(["**** ****", "***1 ****", "**  3****", "* 4 1****",\
+ "     9 5 ", " 6  83  *", "3   1  **", "  8  2***", "  2  ****"])
+    True
+    '''
+    numbers = '123456789'
+
+    for i in range(5):
+        color_numbers = ''
+        for j in range(5):
+            color_numbers += board[8-i-j][i]
+
+        for j in range(1, 5):
+            color_numbers += board[8-i][i+j]
+
+        for number in numbers:
+            if color_numbers.count(number) > 1:
+                return False
+
+    return True
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
